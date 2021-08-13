52c4678aa9e151f8f1b9b40de3bd8cc22b817339
Nicolas Alfaro <nicoalfo@gmail.com>
2021-08-13 03:23:19 -0300

    redireccion y lista de productos

--- js/products.js
+++ js/products.js
@@ -2,5 +2,28 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
-});
+    const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+    fetch(PRODUCTS_URL)
+    .then(respuesta => respuesta.json())
+    .then(datos => { 
+        datos.forEach(datos => {
+            
+            let row = "";
+            row = `
+        <tr>
+            <td>` + datos.name + ` </td>
+            <td>` + datos.description + `</td>
+            <td>` + datos.cost + `</td>
+            <td>` + datos.currency + `</td>
+            <td>` + datos.soldCount + `</td>
+            <td><img heigth="100px" width="100px" src="` + datos.imgSrc + `" alt=""></td>
+        </tr>`
+          
+          document.getElementById("tabla").innerHTML += row;
+        
+        })
+    
+    })
+    
+    .catch(error => alert("Hubo un error:" + error)) 
+})
