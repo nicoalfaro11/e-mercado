8da2726367dcec9a837e8cac33b73e74d52e190a
Nicolas Alfaro <nicoalfo@gmail.com>
2021-10-17 18:50:28 -0300

    entrega 5

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,34 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+function subtotal(i, valor){
+    let subTotal = document.getElementById('cant'+i).value * valor;
+    document.getElementById('sub'+i).innerHTML = subTotal;
+    document.getElementById('cosTot').innerHTML = 'Costo Total: ' + document.getElementById('sub'+i).innerHTML;
+}
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if(resultObj.status === 'ok'){
+            let articulo = resultObj.data;
+            for (let i = 0; i < articulo.articles.length; i++) {
+                if(articulo.articles[i].currency == 'USD'){
+                    var valor = articulo.articles[i].unitCost * 40
+                }else{
+                    var valor = articulo.articles[i].unitCost
+                }
+                let content = "";
+                    content = `
+                    <tbody>
+                        <tr>
+                            <td scope="row"><img src="${articulo.articles[i].src}" width=200px"></td>
+                            <td>${articulo.articles[i].name}</td>
+                            <td><input min="1" id="cant${i}" class="form-control" onchange="subtotal(${i}, ${valor})" type="number" value="${articulo.articles[i].count}"></td>
+                            <td>UYU</td>
+                            <td>${valor}</td>
+                            <td id="sub${i}">${valor * articulo.articles[i].count}</td>
+                        </tr>
+                    </tbody>`;
+                    document.getElementById('articulo').innerHTML += content;
+                    document.getElementById('cosTot').innerHTML = 'Costo Total: ' + document.getElementById('sub'+i).innerHTML;
+            };
+            
+        }
+    })
 });
